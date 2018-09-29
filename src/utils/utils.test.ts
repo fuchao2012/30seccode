285f08f83d63cb26164bb5c8d0f2950f95db67a5
fuchao.zheng <zhengfuchao2008@126.com>
2018-09-29 15:53:58 +0800

    ADD: utils test module

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -5,6 +5,6 @@ describe('utils', () => {
     it('多个函数返回效率最高的一个', () => {
         const fn1 = () => [1, 2, 3, 4, 5, 6, 7, 8, 9, '10'].every(el => typeof el === 'number');
         const fn2 = () => [1, '2', 3, 4, 5, 6, 7, 8, 9, 10].every(el => typeof el === 'number');
-        expect(Utils.mostPerformant([fn1, fn2])).to.equal(1)
+        expect(Utils.mostPerformance([fn1, fn2],10)).to.equal(1)
     });
 });
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -49,28 +49,28 @@ export const getURLParameters = (url: string): any => (url.match(/([^?=&]+)(=(^&
  * hexToRGB('27ae60'); // 'rgb(39, 174, 96)'
  * hexToRGB('#fff'); // 'rgb(255, 255, 255)'
  */
-export const hexToRGB = (hex: string): string => {
-    let alpha = hex.length > 6;
-    let formattedHex = parseInt(hex.length === 3
-        ? hex.slice(hex.startsWith('#') ? 1 : 0).split('').map(c => c + c).join('')
-        : hex
-        , 16);
-    return `rgb${alpha ? 'a' : ''}(${formattedHex >>> (alpha ? 24 : 16)},\
-     ${(formattedHex & (alpha ? 0x00ff0000 : 0x00ff00)) >>> (alpha ? 16 : 8)},\
-     ${((formattedHex & (alpha ? 0x0000ff00 : 0x0000ff)) >>> (alpha ? 8 : 0))}\
-     ${alpha ? ', ' + formattedHex & 0x000000ff : ''})`
-};
+// export const hexToRGB = (hex: string): string => {
+//     let alpha = hex.length > 6;
+//     let formattedHex = parseInt(hex.length === 3
+//         ? hex.slice(hex.startsWith('#') ? 1 : 0).split('').map(c => c + c).join('')
+//         : hex
+//         , 16);
+//     return `rgb${alpha ? 'a' : ''}(${formattedHex >>> (alpha ? 24 : 16)},\
+//      ${(formattedHex & (alpha ? 0x00ff0000 : 0x00ff00)) >>> (alpha ? 16 : 8)},\
+//      ${((formattedHex & (alpha ? 0x0000ff00 : 0x0000ff)) >>> (alpha ? 8 : 0))}\
+//      ${alpha ? ', ' + formattedHex & 0x000000ff : ''})`;
+// };
 
 /**
  * 多个函数哪个效率最高
  * @param fns
  * @param iterations
  */
-export const mostPerformant = (fns: any[], iterations = 10000) => {
+export const mostPerformance = (fns: any[], iterations = 10000) => {
     const times = fns.map(fn => {
-        const before = performance.now();
+        const before = new Date().getTime();
         while (iterations--) fn();
-        return performance.now() - before;
+        return new Date().getTime() - before;
     });
     return times.indexOf(Math.min(...times))
 };
